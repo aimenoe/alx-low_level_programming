74bc9069d370cb14eabaa94f80a3fe41dd7cdc9a
aimenoe <uaimefrancois@gmail.com>
2024-08-27 02:35:57 -0700

    correcting errors

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,21 +4,18 @@
 /**
  * more numbers - prints 10 times the numbers, from 0 to 14
  *
- * return: Always 0.
+ * Return: Always 0.
  */
-
-void more_numbers(void)
-{
+void more_numbers(void) {
 	int i = 0;
 	int first;
 	int second;
 	int results = 0;
-
-	while (i < 0) 
+	while (i < 10)
 	{
 		while (results <= 14)
 		{
-			if (results< 10)
+			if (results < 10)
 			{
 				second = results;
 			}
@@ -26,13 +23,13 @@ void more_numbers(void)
 			{
 				first = results / 10;
 				second = results % 10;
-				_putchar(first + '0';
+				_putchar(first + '0');
 			}
-			_putchar(second + '0';
+			_putchar(second +'0');
 			results ++;
 		}
-		i++
+		i++;
 		results = 0;
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
