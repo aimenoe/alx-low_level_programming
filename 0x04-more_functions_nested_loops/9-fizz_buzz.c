64f642599ebb2fc1af3c4f510450ba2e09c87ea8
aimenoe <uaimefrancois@gmail.com>
2024-08-28 04:28:37 -0700

    Correcting lines of code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("%s", "Fizz");
 		}
@@ -20,11 +24,7 @@ int main(void)
 		{
 			printf("%s", "Buzz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("%s", "FizzBuzz");
-		}
-		else if (i = 1)
+		else if (i == 1)
 		{
 			printf("%d", i);
 		}
