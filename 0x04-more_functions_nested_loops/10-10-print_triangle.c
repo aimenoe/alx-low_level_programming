b6e09f9530cbde118f1b213149498db398a2e2de
aimenoe <uaimefrancois@gmail.com>
2024-08-30 06:55:45 -0700

    Print triangle file

---
+++ 0x04-more_functions_nested_loops/10-10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * Print triangle - printing angle shaped with #
+ * @size: number of # to printed
+ * @i: initial interval range
+ * @j: last interval range
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar("\n");
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(" ");
+			}
+			for (j = 0; i < j; j++)
+			{
+				_putchar("#");
+			}
+			_putchar("\n");
+		}
+	}
+}
