60bf03df09ebe14b8710ab09571594d9df3c0074
aimenoe <uaimefrancois@gmail.com>
2024-09-02 03:52:12 -0700

    Adding print triangle file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * Printing - printing triangle shaped with #
+ * @size: The size of the #
+ * @i: Initial range
+ * @j: Limit range
+ * Return: Always 0.
+ */
+int print_triangle(size)
+{
+	if (size <= size)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j= 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
