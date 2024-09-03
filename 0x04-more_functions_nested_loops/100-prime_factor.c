3e5134036347d3f54146497718125390fffca236
aimenoe <uaimefrancois@gmail.com>
2024-09-03 03:48:05 -0700

    Adding the prime factor file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * Prime factor - printing the largest factor number of the input number.
+ * @n: input
+ * Return: Always 0.
+ */
+int main()
+{
+	long n = 612852475143;
+	long i;
+	long largest_number = 0;
+
+	while (n % 2 == 0)
+	{
+		largest_number = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			largest_number = i;
+			n /= i;
+		}
+	}
+	if (n > 2)
+	{
+		n = largest_number;
+	}
+	printf("%ld\n", largest_number);
+	return (0);
+}
