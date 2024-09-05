ea51d50dc3f0dff2c5d7b2d356750bd2158c8f31
aimenoe <uaimefrancois@gmail.com>
2024-09-05 04:47:02 -0700

    Adding reset to 98 fild

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - updating the pointer with new value.
+ * @*n: holds input data.
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
