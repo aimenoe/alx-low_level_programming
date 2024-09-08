017166c2ee31f7f01d536d35f81f7e2fb6b08f03
aimenoe <uaimefrancois@gmail.com>
2024-09-08 05:30:31 -0700

    Ccorrecting errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@ void print_rev(char *s)
 {
 	int i; length = 0;
 
-	while (*s[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
-	for (i = length -1; i >= 0; i++)
+	for (i = length - 1; i >= 0; i--)
 	{
-		putchar(*s[i]);
+		_putchar(s[i]);
 	}
-	return (0);
+	_putchar('\n');
 }
