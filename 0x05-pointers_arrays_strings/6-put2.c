a39d28fd5eb659be9f78adcbfc5af5d4c37b7ea6
aimenoe <uaimefrancois@gmail.com>
2024-09-09 04:06:45 -0700

    Addingputs file

---
+++ 0x05-pointers_arrays_strings/6-put2.c
@@ -0,0 +1,20 @@
+#inculde "main.h"
+#incude <stdio.h>
+/**
+ * Puts2 - prints the first letter of the word.
+ * @str: contains string
+ * Return: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i];
+		}
+		_putchar('\n');
+	}
+}
