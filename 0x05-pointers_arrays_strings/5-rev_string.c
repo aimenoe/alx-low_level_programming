0ddc6b6ea3a18ce80116b0afd63bdde275df8244
aimenoe <uaimefrancois@gmail.com>
2024-09-09 03:51:15 -0700

    Adding reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - print string in reverse
+ * @s: contained string
+ * Return: return string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		j = s[i];
+		s = s[length] = s[length - 1 - i];
+		s[length - 1 -i] = j;
+	}
+}
