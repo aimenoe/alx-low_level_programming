7ae4527d7bf780da0e52a5bf7747f88babf1c2bd
aimenoe <uaimefrancois@gmail.com>
2024-09-11 03:20:58 -0700

    Adding put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_hallf - print the half of string
+ * @str: contains string
+ * Return: String
+ */
+void puts_half(char *str)
+{
+	int j;
+
+	for (j = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_Putchar(str[i]);
+	}
+	_putchar('\n');
+}
