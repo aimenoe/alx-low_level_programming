25b075d6bc6b950ae552a892eba33cf73bd8f8d1
aimenoe <uaimefrancois@gmail.com>
2024-09-11 23:50:15 -0700

    Correcting typo

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,13 +12,13 @@ int _atoi(char *s)
 
 	do 
 	{
-		if (s == "-")
+		if (*s == '-')
 		{
 			sign *= -1;
 		}
-		else if (s >= "0" && s <= "9")
+		else if (*s >= '0' && *s <= '9')
 		{
-			num = (num * 10) + (s - "0");
+			num = (num * 10) + (s - '0');
 		}
 		else if (num > 0)
 		{
