b3c202a5d64ebd1b6c452bc3f03a215fc926f3ca
aimenoe <uaimefrancois@gmail.com>
2024-09-14 06:27:49 -0700

    Copying string file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies string
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of string
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	for (j = 0; n > j; j++)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
+}
