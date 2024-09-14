b5be323658c04b911949cda27507a1624c8aac1c
aimenoe <uaimefrancois@gmail.com>
2024-09-14 04:08:26 -0700

    Adding concatenatin file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main"
+#include <stdio.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: destinations pointer
+ * @src: sources pointer
+ * Return: Return concatenated strin
+ */
+char *strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != \'0')
+	{
+		dest++;
+	}
+	wile (*src != '\0')
+	{
+		*dest  = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
