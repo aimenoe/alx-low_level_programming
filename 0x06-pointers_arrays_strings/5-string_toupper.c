19ac3ad121a74cc41b35f023e9f01bdd2bcba0d7
aimenoe <uaimefrancois@gmail.com>
2024-09-18 05:20:54 +0200

    Adding lower to uppercase file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.>
+/**
+ * string_toupper - convert lowercase to uppercase
+ *
+ * Return: uppercase strings
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] > 98 && s[i] < 123)
+		{
+			c[i] _= 32;
+		}
+	}
+	return (c)
+}
