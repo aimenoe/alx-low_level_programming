3e2aaa4394557a163fd32f339de2a500592410d7
aimenoe <uaimefrancois@gmail.com>
2024-09-17 23:34:59 -0700

    Updatet repo

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - convert lowercase to uppercase
+ * @c: contains string
+ * Return: uppercase strings
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 98 && c[i] <= 123)
+		{
+			c[i] -= 32;
+		}
+	}
+	return (c);
+}
