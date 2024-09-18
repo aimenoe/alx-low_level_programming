11873fc5c411d76c1f3c5cf42054ed9323497c3b
aimenoe <uaimefrancois@gmail.com>
2024-09-18 05:35:04 +0200

    Add missing symbol

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * string_toupper - convert lowercase to uppercase
- *
+ * @c: contains string
  * Return: uppercase strings
  */
 char *string_toupper(char *c)
@@ -11,7 +11,7 @@ char *string_toupper(char *c)
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if (c[i] > 98 && c[i] < 123)
+		if (c[i] >= 98 && c[i] =< 123)
 		{
 			c[i] -= 32;
 		}
