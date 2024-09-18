9c42b18c66e9b675df425ec1254d70d62c3e23cb
aimenoe <uaimefrancois@gmail.com>
2024-09-18 03:19:43 -0700

    Adding encode rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes string with rot13
+ * @c: contains string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *c)
+{
+	int i;
+	int j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "nopqrstuvwxyzabcedfghijklmNOPQRSTUVWZABACDEFGHIJKLM";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
