6e41427a696db4b87f1d23cde414a4363a2b7503
aimenoe <uaimefrancois@gmail.com>
2024-09-17 23:49:22 -0700

    Add encode file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - encodes a string into 1337
+ * @c: string to be encoded
+ * Return: return string
+ */
+char *leet(char *c)
+{
+	int i;
+	int j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
