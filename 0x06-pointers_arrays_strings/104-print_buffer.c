4528327e8952eb4eaf7eed1a35a4f0b43211c6e6
aimenoe <uaimefrancois@gmail.com>
2024-09-18 23:52:14 -0700

    Adding buffer printing file

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - print a buffer
+ * @b: buffer pointer
+ * @sizze: size of buffer
+ * Return: return buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (i < size)
+	{
+		j = size - i < 10 ? size - i : 10;
+		printf("%08x: ", i);
+		for (k = 0; k < 10; k++)
+		{
+			if (k < j)
+			{
+				printf("%02x", *(b + i + k));
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		for (k = 0; k < j; K++)
+		{
+			int c = *(b + i + k);
+
+			if (c < 32 || c > 132)
+			{
+				c = ".";
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		i += 10;
+	}
+}
